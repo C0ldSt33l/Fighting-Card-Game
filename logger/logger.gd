2d4cac086a8054906ccac7344bf5ce8d52e13df1
C0ldSt33l <sychev2003di@gmail.com>
2025-02-22 18:11:52 +0300

    refactor `Logger`

--- logger/logger.gd
+++ logger/logger.gd
@@ -3,7 +3,7 @@ extends RichTextLabel
 
 @export_category('Text Colors')
 @export var TEXT_DEFAULT_COLOR := Color.GRAY
-@export var TYPE_COLOR := Color.YELLOW
+@export var OBJ_TYPE_COLOR := Color.YELLOW
 @export var OBJ_NAME_COLOR := Color.WHITE
 @export var SCORE_OLD_VAL_COLOR := Color.WHITE
 @export var SCORE_NEW_VAL_COLOR := Color.WHITE
@@ -12,7 +12,18 @@ extends RichTextLabel
 @export var EFFECT_NAME_COLOR := Color.ORANGE
 @export var DMG_COLOR := Color.RED
 
-var OBJ_PLACEHOLDER := 'Obj %s type of %s' % [colorful('%s', OBJ_NAME_COLOR), colorful('%s', TYPE_COLOR)]
+const OBJ_PLACEHOLDER := "%s '%s'"
+const OBJ_HAS_CREATED_PLACEHOLDER := OBJ_PLACEHOLDER + ' has created'
+const OBJ_HAS_DESTROYED_PLACEHOLDER := OBJ_PLACEHOLDER + ' has destroyed'
+
+const CARD_HAS_PLAYED_PLACEHOLDER := "%s '%s' has played with effects:"
+const COMBO_HAS_ACTIVATED_PLACEHOLDER := "%s '%s' has activated with effects:"
+const APPLYED_EFFECT_PLACEHOLDER := '%s'
+
+const NUMBER_CHANGE_PLACEHOLDER := '%s -> %s: %s'
+const SCORE_POINTS_CHANGE_PLACEHOLDER := 'Score: points ' + NUMBER_CHANGE_PLACEHOLDER
+const SCORE_MULTIPLIER_CHANGE_PLACEHOLDER := 'Score: multiplier ' + NUMBER_CHANGE_PLACEHOLDER
+const SCORE_TOTAL_SCORE_CHANGE_PLACEHOLDER := 'Score: total score ' + NUMBER_CHANGE_PLACEHOLDER
 
 '''
 Possible colorful log elements:
@@ -38,53 +49,81 @@ func put_text(text: String) -> void:
 
 
 func obj_has_created_log(obj: Variant) -> void:
-	var name_and_type := self.get_obj_name_and_type(obj)
-	self.put_text((OBJ_PLACEHOLDER + ' has created') % name_and_type) 
+	var type_and_name := self.get_obj_type_and_name(obj)
+	self.put_text(OBJ_HAS_CREATED_PLACEHOLDER % [
+		colorful(type_and_name[0], OBJ_TYPE_COLOR),
+		colorful(type_and_name[1], OBJ_NAME_COLOR),
+	]) 
 
 
 func change_points_log(old: int, new: int, diff: int) -> void:
 	var diff_color := SCORE_NEGATIVE_DIFF_COLOR if diff < 0 else SCORE_DIFF_COLOR
-	var placeholder := 'Score change: points %s -> %s: %s' % [colorful('%d', SCORE_OLD_VAL_COLOR), colorful('%d', SCORE_NEW_VAL_COLOR), colorful('%d', diff_color)]
-	self.put_text(placeholder % [old, new, diff])
+	self.put_text(SCORE_POINTS_CHANGE_PLACEHOLDER % [
+		colorful(str(old), SCORE_OLD_VAL_COLOR),
+		colorful(str(new), SCORE_NEW_VAL_COLOR),
+		colorful(str(diff), diff_color),
+	])
 
 
 func change_multiplier_log(old: int, new: int, diff: int) -> void:
-	var diff_color := Color.RED if diff < 0 else Color.GREEN
-	var placeholder := 'Score change: multiplier %s -> %s: %s' % [colorful('%d', SCORE_OLD_VAL_COLOR), colorful('%d', SCORE_NEW_VAL_COLOR), colorful('%d', diff_color)]
-	self.put_text(placeholder % [old, new, diff])
+	var diff_color := SCORE_NEGATIVE_DIFF_COLOR if diff < 0 else SCORE_DIFF_COLOR
+	self.put_text(SCORE_MULTIPLIER_CHANGE_PLACEHOLDER % [
+		colorful(str(old), SCORE_OLD_VAL_COLOR),
+		colorful(str(new), SCORE_NEW_VAL_COLOR),
+		colorful(str(diff), diff_color),
+	])
 
 
 func change_total_score_log(old: int, new: int, diff: int) -> void:
-	var diff_color := Color.RED if diff < 0 else Color.GREEN
-	var placeholder := 'Score change: total score %s -> %s: %s' % [colorful('%d', SCORE_OLD_VAL_COLOR), colorful('%d', SCORE_NEW_VAL_COLOR), colorful('%d', diff_color)]
-	self.put_text(placeholder % [old, new, diff])
+	var diff_color := SCORE_NEGATIVE_DIFF_COLOR if diff < 0 else SCORE_DIFF_COLOR
+	self.put_text(SCORE_TOTAL_SCORE_CHANGE_PLACEHOLDER % [
+		colorful(str(old), SCORE_OLD_VAL_COLOR),
+		colorful(str(new), SCORE_NEW_VAL_COLOR),
+		colorful(str(diff), diff_color),
+	])
 
 
-func card_has_played_log(card: Card) -> void:
-	var placeholder := colorful('Card ', TYPE_COLOR) + colorful('%s', OBJ_NAME_COLOR) + ' has dealt dmg: ' + colorful('%d', DMG_COLOR)
-	self.put_text(placeholder % [card.card_name, card.dmg])
+func card_has_played_log(c: Card) -> void:
+	var type_and_name := self.get_obj_type_and_name(c)
+	self.put_text(CARD_HAS_PLAYED_PLACEHOLDER % [
+		colorful(type_and_name[0], OBJ_TYPE_COLOR),
+		colorful(type_and_name[1], OBJ_NAME_COLOR),
+		colorful(str(c.dmg), DMG_COLOR),
+	])
+	for e in c.effects:
+		self.put_text(APPLYED_EFFECT_PLACEHOLDER % [colorful(e.name, EFFECT_NAME_COLOR)])
 
 
-func combo_has_activated(combo: Combo) -> void:
-	var placeholder := colorful('Combo', TYPE_COLOR) + colorful(' %s ', OBJ_NAME_COLOR) + 'has activated'
-	self.put_text(placeholder % [combo.name])
+func combo_has_activated(c: Combo) -> void:
+	var type_and_name := self.get_obj_type_and_name(c)
+	self.put_text(COMBO_HAS_ACTIVATED_PLACEHOLDER % [
+		colorful(type_and_name[0], OBJ_TYPE_COLOR),
+		colorful(type_and_name[1], OBJ_NAME_COLOR),
+	])
+	self.put_text(('Main ' + APPLYED_EFFECT_PLACEHOLDER) % [colorful(c.main_effect.name, EFFECT_NAME_COLOR)])
+	for e in c.effects:
+		self.put_text(APPLYED_EFFECT_PLACEHOLDER % [colorful(e.name, EFFECT_NAME_COLOR)])
+
 
 
 func obj_has_destroyed_log(obj: Variant) -> void:
-	var name_and_type := self.get_obj_name_and_type(obj)
-	self.put_text((OBJ_PLACEHOLDER + ' has destroyed') % name_and_type)
+	var type_and_name := self.get_obj_type_and_name(obj)
+	self.put_text(OBJ_HAS_DESTROYED_PLACEHOLDER % [
+		colorful(type_and_name[0], OBJ_TYPE_COLOR),
+		colorful(type_and_name[1], OBJ_NAME_COLOR),
+	])
 
 
-func get_obj_name_and_type(obj: Variant) -> Array[String]:
-	var obj_class: String = obj.get_script().get_global_name()
+func get_obj_type_and_name(obj: Variant) -> Array[String]:
+	var obj_type: String = obj.get_script().get_global_name()
 	var obj_name: String
-	match obj_class:
+	match obj_type:
 		'Card':
 			obj_name = (obj as Card).card_name
 		_:
 			obj_name = obj.name
 
-	return [obj_name, obj_class]
+	return [obj_type, obj_name]
 
 
 static func colorful(text: String, c: Color) -> String:
