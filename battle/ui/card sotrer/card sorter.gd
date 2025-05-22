40a4c1fb52ec6b4f72c4a00e0ad8da6773c01f67
C0ldSt33l <sychev2003di@gmail.com>
2025-05-22 20:45:47 +0300

    rename `CardFilter` to `CardSorter`

--- battle/ui/card sotrer/card sorter.gd
+++ battle/ui/card sotrer/card sorter.gd
@@ -1,5 +1,5 @@
 extends Control
-class_name CardFilter
+class_name CardSorter 
 
 @onready var filter_btn: Button = $"VBoxContainer/Filter button" as Button
 @onready var filter_modes: HBoxContainer = $"VBoxContainer/Filter modes" as HBoxContainer
@@ -10,6 +10,26 @@ var filter_mode_btns: Array[Button] :
 			return btns
 var filter_mode_grp: ButtonGroup = ButtonGroup.new()
 
+var filter_func: Callable
+
+
 func _ready() -> void:
+	self.filter_modes.queue_sort()
 	for b in self.filter_mode_btns:
 		b.button_group = self.filter_mode_grp
+		b.custom_minimum_size.x = self.size.x / 2
+
+
+func _on_by_value_pressed() -> void:
+	self.filter_func = self.sort_cards_by_value
+
+
+func _on_by_type_pressed() -> void:
+	self.filter_func = self.sort_cards_by_type
+
+static func sort_cards_by_value(f: Card, s: Card) -> bool:
+	return f.point > s.point
+
+
+static func sort_cards_by_type(f: Card, s: Card) -> bool:
+	return false
