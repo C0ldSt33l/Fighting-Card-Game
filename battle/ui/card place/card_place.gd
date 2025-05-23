6f7f352a9fedf2ff3015f5765129330649cfb858
C0ldSt33l <sychev2003di@gmail.com>
2025-05-23 20:56:56 +0300

    change drag n drop signals and fix centering for `CardPlace`

--- battle/ui/card place/card_place.gd
+++ battle/ui/card place/card_place.gd
@@ -7,15 +7,24 @@ const SIZE: Vector2i = Vector2i(40, 40)
 
 
 func _ready() -> void:
+	Events.drag_completed.connect(self.on_drag_completed)
 	self.panel.check = func(data: Variant) -> bool:
 		return data is Card
 	self.custom_minimum_size = SIZE
 
 
 func get_card() -> Card:
-	return self.panel.held_data_data
+	return self.panel.held_data
 
 
 func remove_card() -> void:
-	self.panel.remove_child(self.panel.held_data_data)
-	self.panel.held_data_data = null
+	self.panel.remove_child(self.panel.held_data)
+	self.panel.held_data = null
+
+
+func on_drag_completed(data: Variant, where: Variant) -> void:
+	if self != where: return
+	var c: Card = data.duplicate()
+	self.panel.add_child(c)
+	c.size = c.background.size
+	c.set_anchors_and_offsets_preset(PRESET_CENTER, PRESET_MODE_KEEP_SIZE)
