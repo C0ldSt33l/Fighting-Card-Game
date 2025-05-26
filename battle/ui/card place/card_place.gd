96c6ae2bdb2e178b7aba7b971c9635a19cf79745
C0ldSt33l <sychev2003di@gmail.com>
2025-05-26 20:59:17 +0300

    on drop card place assings index to droped card

--- battle/ui/card place/card_place.gd
+++ battle/ui/card place/card_place.gd
@@ -5,6 +5,7 @@ const SIZE: Vector2i = Vector2i(40, 40)
 
 @onready var panel: ObjectDropable = $Panel as ObjectDropable 
 var index: int = -1
+var card: Card = null
 
 
 func _ready() -> void:
@@ -18,6 +19,15 @@ func get_card() -> Card:
 	return self.panel.held_data
 
 
+func add_card(c: Card) -> void:
+	self.panel.add_child(c)
+	c.size = self.card.background.size
+	c.set_anchors_and_offsets_preset(PRESET_CENTER, PRESET_MODE_KEEP_SIZE)
+
+	self.card = c
+	c.index = self.index
+
+
 func remove_card() -> void:
 	self.panel.remove_child(self.panel.held_data)
 	self.panel.held_data = null
@@ -25,7 +35,6 @@ func remove_card() -> void:
 
 func on_drag_completed(data: Variant, where: Variant) -> void:
 	if self != where: return
-	var c: Card = data
-	self.panel.add_child(c)
-	c.size = c.background.size
-	c.set_anchors_and_offsets_preset(PRESET_CENTER, PRESET_MODE_KEEP_SIZE)
+
+	self.add_card(data)
+
