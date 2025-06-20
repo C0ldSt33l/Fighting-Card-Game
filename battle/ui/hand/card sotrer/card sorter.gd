f2ab853ea4f3f92a23e8bb6b419d8981625bed8f
C0ldSt33l <sychev2003di@gmail.com>
2025-06-21 01:09:05 +0300

    change sort and add sort by type

--- battle/ui/hand/card sotrer/card sorter.gd
+++ battle/ui/hand/card sotrer/card sorter.gd
@@ -16,12 +16,34 @@ func _ready() -> void:
 	pass
 
 func _on_by_value_pressed() -> void:
-	self.card_sorted.emit(self.sort_cards_by_value)
+	self.card_sorted.emit(__sort_by_value)
 
 
 func _on_by_type_pressed() -> void:
-	self.card_sorted.emit(self.sort_cards_by_type)
+	self.card_sorted.emit(__sort_by_type)
 
 
-static func sort_cards_by_value(f: Card, s: Card) -> bool:
+static func __sort_by_value(cards: Array[Card]) -> void:
+	cards.sort_custom(__sort_cards_by_point)
+
+static func __sort_by_type(cards: Array[Card]) -> void:
+	cards.sort_custom(__sort_cards_by_type)
+	var sep: int = 0
+	var a: Array[Array] = []
+	for i in range(1, cards.size()):
+		var cur: Card = cards[i]
+		var prev: Card = cards[i - 1]
+		if cur._material != prev._material:
+			a.append(cards.slice(sep, i))
+			a[-1].sort_custom(__sort_cards_by_point)
+			sep = i 
+	cards.clear()
+	for cs in a:
+		cards.append_array(cs)
+
+
+static func __sort_cards_by_point(f: Card, s: Card) -> bool:
 	return f.point > s.point
+
+static func __sort_cards_by_type(f: Card, s: Card) -> bool:
+	return f._material > s._material
